4277044262490f12b23fa846ff15429e5b748934
Guilherme <guilhermenunes09@protonmail.com>
2021-01-06 19:28:05 -0200

    Application

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,15 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'mongoid'
+Mongoid.load!(File.expand_path('mongoid.yml', './config'))
 
 module DebtsAppCode7
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
